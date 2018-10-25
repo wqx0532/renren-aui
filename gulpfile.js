017e5462e4635e3ee95ed364931e324d42ce5a3a
daxiongYang <daxiong.yang@qq.com>
2018-10-25 10:01:23 +0800

    element-theme多个打包编译

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
-var gulp = require('gulp')
-var $    = require('gulp-load-plugins')()
-var del  = require('del')
+var gulp   = require('gulp')
+var $      = require('gulp-load-plugins')()
+var fs     = require('fs')
+var path   = require('path')
+var del    = require('del')
+var colors = require('colors')
+var child_process = require('child_process')
 
 var env = 'dev' // 环境变量
 
@@ -24,10 +28,10 @@ gulp.task('copyFiles', () => {
  */
 gulp.task('pages', () => {
   gulp.src(['./src/pages/**/*.html'])
+    .pipe($.htmlTagInclude())
     .pipe($.if(env === 'prod', $.replace(/src=\"(.*)\/vue.js\"/g, (match, p1) => {
       return `src="${p1}/vue.min.js"`
     })))
-    .pipe($.htmlTagInclude())
     .pipe(gulp.dest('./dist/pages'))
     .pipe($.connect.reload())
 })
@@ -130,4 +134,69 @@ gulp.task('build', () => {
   del.sync(['./dist/**'])
   env = 'prod'
   gulp.start(['copyFiles', 'pages', 'styles', 'skins', 'images', 'scripts'])
+})
+
+/**
+ * element主题
+ */
+gulp.task('element-theme', () => {
+  var themeList        = require('./src/skins.json').filter(item => !item.hasBuild)
+  var styleFileDir     = './src/scss'
+  var styleFileDirTemp = `${styleFileDir}-temp`
+  var themeFileDir     = './src/element-theme'
+  var et               = require('element-theme')
+  var etOptions        = require('./package.json')['element-theme']
+  
+  if (themeList.length <= 0) { return del(styleFileDirTemp) }
+
+  // 删除临时文件，保证本次操作正常执行
+  del(styleFileDirTemp)
+
+  // 拷贝一份scss样式文件夹，作为构建的临时处理文件夹
+  child_process.spawnSync('cp', ['-r', styleFileDir, styleFileDirTemp])
+
+  // 开始构建生成
+  fnCreate(themeList)
+
+  function fnCreate (themeList) {
+    if (themeList.length >= 1) {
+      console.log('\n')
+      console.log(colors.green('-------------------- 待构建，主题 -------------------------'))
+      console.log(themeList)
+      console.log('\n')
+      console.log(colors.green('-------------------- 构建中，主题 -------------------------'))
+      console.log(themeList[0])
+      console.log('\n')
+
+      // 修改.scss临时文件中的$--color-primary主题变量值
+      var data = fs.readFileSync(etOptions.config, 'utf8')
+      var result = data.replace(/\$--color-primary:(.*) !default;/, `$--color-primary:${themeList[0].color} !default;`)
+      fs.writeFileSync(path.resolve(etOptions.config), result)
+
+      // 调用element-theme插件，生成element组件主题
+      etOptions.out = `${themeFileDir}/${themeList[0].name}`
+      et.run(etOptions, () => {
+        themeList.splice(0, 1)
+        fnCreate(themeList)
+      })
+    } else {
+      // 删除临时文件
+      del(styleFileDirTemp)
+      console.log('\n')
+      console.log(colors.green('-------------------- 构建完毕，删除临时文件 -------------------------'))
+      console.log(styleFileDirTemp)
+      console.log('\n')
+      
+      // 删除主题不需要的部分文件
+      var files = [
+        `${themeFileDir}/**/*.css`,
+        `!${themeFileDir}/**/index.css`,
+        `!${themeFileDir}/**/fonts`
+      ]
+      del(files)
+      console.log(colors.green('-------------------- 构建完毕，删除主题独立组件文件 -------------------------'))
+      console.log(files)
+      console.log('\n')
+    }
+  }
 })
