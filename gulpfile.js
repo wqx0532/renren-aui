703ba08af1b2f4f00b3e296690f16681a8ae02d5
daxiongYang <daxiong.yang@qq.com>
2018-10-31 17:49:57 +0800

    样式调整

--- gulpfile.js
+++ gulpfile.js
@@ -8,11 +8,6 @@ var child_process = require('child_process')
 
 var env  = 'dev'  // 环境变量
 
-var isCreateAuiSkin  = false
-var styleFileDir     = './src/scss'
-var styleFileDirTemp = `${styleFileDir}-temp`
-var skin             = 'blue'
-
 /**
  * 复制文件
  */
@@ -45,13 +40,31 @@ gulp.task('pages', () => {
  * 样式
  */
 gulp.task('styles', () => {
-  return gulp.src([`${isCreateAuiSkin ? styleFileDirTemp : styleFileDir}/aui.scss`])
+  return gulp.src(['./src/scss/aui.scss'])
     .pipe($.sass().on('error', $.sass.logError))
     .pipe($.autoprefixer({
       browsers: require('./package.json')['element-theme'].browsers,
       cascade: false
     }))
-    .pipe($.rename({ suffix: `-${skin}` }))
+    .pipe($.rename({ suffix: '-blue' }))
+    .pipe(gulp.dest('./dist/css'))
+    .pipe($.cleanCss())
+    .pipe($.rename({ suffix: '.min' }))
+    .pipe(gulp.dest('./dist/css'))
+    .pipe($.connect.reload())
+})
+
+/**
+ * 皮肤
+ */
+gulp.task('skins', () => {
+  return gulp.src(['./src/scss/skins/**/*.scss'])
+    .pipe($.sass().on('error', $.sass.logError))
+    .pipe($.autoprefixer({
+      browsers: require('./package.json')['element-theme'].browsers,
+      cascade: false
+    }))
+    .pipe($.rename({ prefix: 'aui-' }))
     .pipe(gulp.dest('./dist/css'))
     .pipe($.cleanCss())
     .pipe($.rename({ suffix: '.min' }))
@@ -97,7 +110,7 @@ gulp.task('scripts', () => {
  */
 gulp.task('watch', () => {
   gulp.watch(['./src/pages/**/*.html', './src/templates/**/*.html'], ['pages'])
-  gulp.watch(['./src/scss/**/*.{scss,css}'], ['styles', 'create-aui-skin'])
+  gulp.watch(['./src/scss/**/*.{scss,css}'], ['styles', 'skins'])
   gulp.watch(['./src/img/**/*.{gif,jpeg,jpg,png}'], ['images'])
   gulp.watch(['./src/js/**/*.js'], ['scripts'])
 })
@@ -116,13 +129,13 @@ gulp.task('webserver', () => {
 gulp.task('serve', () => {
   del.sync(['./dist/**'])
   env = 'dev'
-  gulp.start(['copyFiles', 'pages', 'styles', 'images', 'scripts', 'watch', 'webserver', 'create-aui-skin'])
+  gulp.start(['copyFiles', 'pages', 'styles', 'skins', 'images', 'scripts', 'watch', 'webserver'])
 })
 
 gulp.task('build', () => {
   del.sync(['./dist/**'])
   env = 'prod'
-  gulp.start(['copyFiles', 'pages', 'styles', 'images', 'scripts',  'create-aui-skin'])
+  gulp.start(['copyFiles', 'pages', 'styles', 'skins', 'images', 'scripts'])
 })
 
 /**
@@ -184,56 +197,6 @@ gulp.task('create-element-theme', () => {
       del.sync(files)
       console.log(colors.green('-------------------- 构建完毕，删除主题多余文件 -------------------------'))
       console.log(files)
-      console.log('\n')
-    }
-  }
-})
-
-/**
- * 创建aui皮肤
- */
-gulp.task('create-aui-skin', () => {
-  var variablesDir     = `${styleFileDirTemp}/variables.scss`
-  var skinList         = require('./src/skins.json').filter(item => !item.hasBuild)
-  if (skinList.length <= 0) { return del.sync(styleFileDirTemp) }
-
-  // 删除临时文件，保证本次操作正常执行
-  del.sync(styleFileDirTemp)
-
-  // 拷贝一份scss样式文件夹，作为构建的临时处理文件夹
-  child_process.spawnSync('cp', ['-r', styleFileDir, styleFileDirTemp])
-  
-  // 开始构建生成
-  isCreateAuiSkin = true
-  fnCreate(skinList)
-
-  function fnCreate (skinList) {
-    if (skinList.length >= 1) {
-      console.log('\n')
-      console.log(colors.green('-------------------- 待构建，皮肤 -------------------------'))
-      console.log(skinList)
-      console.log('\n')
-      console.log(colors.green('-------------------- 构建中，皮肤 -------------------------'))
-      console.log(skinList[0])
-      console.log('\n')
-
-      // 修改variables.scss文件中的$--color-primary皮肤变量值
-      var data = fs.readFileSync(variablesDir, 'utf8')
-      var result = data.replace(/\$--color-primary:(.*) !default;/, `$--color-primary:${skinList[0].color} !default;`)
-      fs.writeFileSync(path.resolve(variablesDir), result)
-
-      // 调用stylesTask，生成aui皮肤
-      skin = skinList[0].name
-      gulp.start(['styles'], () => {
-        skinList.splice(0, 1)
-        fnCreate(skinList)
-      })
-    } else {
-      // 删除临时文件
-      del.sync(styleFileDirTemp)
-      console.log('\n')
-      console.log(colors.green('-------------------- 构建完毕，删除临时文件 -------------------------'))
-      console.log(styleFileDirTemp)
       console.log('\n')
     }
   }
